042c833b61de205cfaf409e4170e1e7beeca0c7c
SanderDevMaybe <mikolajkim585@gmail.com>
2025-08-13 04:20:37 +0200

    Update Boombox.lua

--- Boombox.lua
+++ Boombox.lua
@@ -133,31 +133,43 @@ stroke.Parent = Main
 
 local RunService = game:GetService("RunService")
 
-Title.Name = "Title"
-Title.Parent = Main
-Title.BackgroundColor3 = Color3.fromRGB(0, 255, 255)
-Title.BackgroundTransparency = 1.000
-Title.Position = UDim2.new(0.0339285731, 0, 0.0564263314, 0)
-Title.Size = UDim2.new(0, 200, 0, 23)
-Title.Font = Enum.Font.GothamSemibold
-Title.Text = text
-Title.TextColor3 = Color3.fromRGB(255, 0, 0) -- ustawiamy bazowy czerwony, bo gradient go nadpisze
-Title.TextSize = 12.000
-Title.TextXAlignment = Enum.TextXAlignment.Left
+local frame = Main  
+
+local demonicLabel = Instance.new("TextLabel")
+demonicLabel.Parent = frame
+demonicLabel.BackgroundTransparency = 1
+demonicLabel.Position = UDim2.new(0.0339, 0, 0.0564, 0)
+demonicLabel.Size = UDim2.new(0, 80, 0, 23)
+demonicLabel.Font = Enum.Font.GothamSemibold
+demonicLabel.RichText = true
+demonicLabel.Text = "Demonic"
+demonicLabel.TextSize = 27
+demonicLabel.TextXAlignment = Enum.TextXAlignment.Left
+demonicLabel.TextColor3 = Color3.fromRGB(255, 0, 0) 
 
 local gradient = Instance.new("UIGradient")
-gradient.Parent = Title
+gradient.Parent = demonicLabel
 gradient.Color = ColorSequence.new{
     ColorSequenceKeypoint.new(0, Color3.fromRGB(150, 0, 0)),
     ColorSequenceKeypoint.new(0.5, Color3.fromRGB(255, 200, 200)),
     ColorSequenceKeypoint.new(1, Color3.fromRGB(150, 0, 0)),
 }
 
--- Animacja falującego połysku
 RunService.RenderStepped:Connect(function()
     gradient.Offset = Vector2.new(math.sin(tick() * 2) * 0.5, 0)
 end)
 
+local songsLabel = Instance.new("TextLabel")
+songsLabel.Parent = frame
+songsLabel.BackgroundTransparency = 1
+songsLabel.Position = UDim2.new(0.25, 0, 0.03, 0)
+songsLabel.Size = UDim2.new(0, 80, 0, 23)
+songsLabel.Font = Enum.Font.GothamSemibold
+songsLabel.RichText = true
+songsLabel.Text = "     Songs"
+songsLabel.TextSize = 27
+songsLabel.TextXAlignment = Enum.TextXAlignment.Left
+songsLabel.TextColor3 = Color3.new(1, 1, 1) 
 
 
     DragFrame.Name = "DragFrame"
